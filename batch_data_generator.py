c37c89f9a23965a7ee505b624c5c6cd27f1ee643
Jv Kyle Eclarin <jvkyleeclarin@gmail.com>
2019-04-13 01:14:28 -0500

    added groundwork for python file

--- batch_data_generator.py
+++ batch_data_generator.py
@@ -1,7 +1,7 @@
 '''
 The purpose of this file is to use Python to make the images to train AI models with.
 
-Dependencies: python 3.5+, numpy, matplotlib
+Dependencies: python 3.6.2, numpy, matplotlib
 
 Python is a bit more accessible than Matlab; therefore, this file will be better for
 making training images in the long run.
@@ -9,10 +9,12 @@ making training images in the long run.
 
 
 from PIL import Image
-import matplotlib.pyplot as pltfrom os import listdir
+import matplotlib.pyplot as plt
+import os
+from os import listdir
 from os.path import isfile, join
 
-%matplotlib inline
+# %matplotlib inline
 
 # load necessary files
 cwd = os.getcwd()
@@ -23,7 +25,6 @@ shapes_path = join(cwd, 'assets', 'photoshop files')
 shapes = [f for f in listdir(shapes_path) if isfile(join(mypath, f))]
 
 
-# iterate over background images
 alphanumeric_vector = ['A', 'B', 'C', 'D', 'E', 'F', \
                        'G', 'H', 'I', 'J', 'K', 'L', \
                        'M', 'N', 'O', 'P', 'Q', 'R', \
@@ -31,6 +32,8 @@ alphanumeric_vector = ['A', 'B', 'C', 'D', 'E', 'F', \
                        'Y', 'Z', '1', '2', '3', '4', \
                        '5', '6', '7', '8', '9']
 
+
+# choose random alphanumeric, and , 
 
 # create xml files
 
